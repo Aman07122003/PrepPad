0da518c38562a485c809f6ecaef07e00295ced7a
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-09-08 13:47:57 +0530

    chore: add environment configuration and polyfill files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import EmployeeRegister from './pages/EmployeeRegister.jsx';
+import EmployeeProfile from './pages/EmployeeProfile.jsx';
+import Employee from './pages/Employee.jsx';
+import Companies from './pages/Companies.jsx';
+import CompanyRegisteration from './pages/CompanyRegisteration.jsx';
+import Home from './pages/Home.jsx';
+import GitRepo from './pages/GitRepo.jsx';
+import Notes from './pages/Notes.jsx';
+import CreateNote from './pages/CreateNote.jsx';
+import NoteDetail from './pages/NoteDetail.jsx';
 
 const App = () => {
   return (
-    <div className='h-screen w-full'>
-      <div>hellow</div>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register-employee" element={<EmployeeRegister />} />
+        <Route path="/employees/:id" element={<EmployeeProfile />} />
+        <Route path="/employees" element={<Employee />} />
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/companies/new" element={<CompanyRegisteration />} />
+        <Route path='/git-repo' element={<GitRepo/>}/>
+        <Route path='/notes' element={<Notes/>}/>
+        <Route path='/notes/new' element={<CreateNote/>}/>
+        <Route path='/notes/canva' element={<Canva/>}/>
+        <Route path='/notes/dsa/graph/intro' element={<Intro/>}/>
+        <Route path="/notes/:category/:subCategory/:question" element={<NoteDetail />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
