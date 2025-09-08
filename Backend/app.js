bfb5ac461444b446406b0ff19138123dccf4f2d6
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-09-08 17:18:30 +0530

    image path updated

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,8 @@ const __dirname = path.dirname(__filename);
 
 app.use('/api/notes', notesRoutes); 
 app.use("/files", express.static(path.join(process.cwd(), "Files")));
-app.use('/static', express.static(path.join(__dirname, 'Data/Graph/Media')));
+app.use('/static', express.static(path.join(process.cwd(), 'Data/Graph/Media')));
+
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
   });
